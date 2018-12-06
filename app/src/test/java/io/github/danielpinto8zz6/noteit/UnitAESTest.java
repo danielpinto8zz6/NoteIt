395a91f6eabd3b17fc5240d4d03b6e9e8b8ec26b
Raul de la Fuente <raul.delafuentec@gmail.com>
2018-12-06 11:42:03 +0100

    New JUnit Tests AES

--- app/src/test/java/io/github/danielpinto8zz6/noteit/UnitAESTest.java
+++ app/src/test/java/io/github/danielpinto8zz6/noteit/UnitAESTest.java
@@ -3,6 +3,9 @@ package io.github.danielpinto8zz6.noteit;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.UnsupportedEncodingException;
 
 import io.github.danielpinto8zz6.noteit.encryption.AESHelper;
@@ -11,29 +14,28 @@ import static org.junit.Assert.assertEquals;
 
 public class UnitAESTest {
 
-    private final String msg = "123456";
-    private final String key = "abcdef";
-    private final String iv = "ABCDEF";
-    private final String key2 = "@#hello123$";
-    private final byte[] bMsg = msg.getBytes();
-    private final byte[] bKey = key.getBytes();
-    private final byte[] bIv = iv.getBytes();
-    private byte[] ans = new byte[0];
-    private byte[] ans2 = new byte[0];
-    private byte[] bAns = new byte[0];
-    private String ansBase64 = null;
-    private String deansBase64 = null;
-    private byte[] deByteAns = new byte[0];
+    private final String path = "C:\\DATOS\\Universidad\\3-Tercero\\1\\GPS\\Demo-Note-it\\NoteIt\\app\\src\\main\\res\\EncriptionTestFile.txt";
+    private final String password = "@#hello123$";
+    private final String pathEncrypt = "C:\\DATOS\\Universidad\\3-Tercero\\1\\GPS\\Demo-Note-it\\NoteIt\\app\\src\\main\\res\\EncriptionTestFile.txt.crypt";
+    private final String outPath = "C:\\DATOS\\Universidad\\3-Tercero\\1\\GPS\\Demo-Note-it\\NoteIt\\app\\src\\main\\res\\EncriptionTestFileOut.txt";
+    private final File file = new File(path);
+    private static String text = "";
+    private static String textOut = "";
+
 
     @Before
     public void setUpEncryption() {
         try {
-            ans = AESHelper.encrypt(iv, key, msg.getBytes());
-            ans2 = AESHelper.encrypt(iv, key2, msg.getBytes());
-            ansBase64 = AESHelper.encryptStrAndToBase64(iv, key, msg);
-            bAns = AESHelper.encrypt(bIv, bKey, bMsg);
-            new String(ans, "UTF-8");
-            new String(ans, "UTF-8");
+            //Read the content of the file to be encrypted
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            while ((br.readLine() != null)) {
+                text = text + br.readLine();
+            }
+            br.close();
+
+            //Encrypt
+            AESHelper.encryptfile(path, password);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -42,8 +44,19 @@ public class UnitAESTest {
     @Test
     public void Encryption_isCorrect() {
         try {
-            byte[] deans = AESHelper.decrypt(iv, key, ans);
-            assertEquals(msg, new String(deans, "UTF-8"));
+            //Decrypt
+            AESHelper.decrypt(pathEncrypt, password, outPath);
+
+            File fileOut = new File (outPath);
+
+            BufferedReader br = new BufferedReader(new FileReader(fileOut));
+            while ((br.readLine() != null)) {
+                textOut = textOut + br.readLine();
+            }
+            br.close();
+
+            assertEquals(text, textOut);
+
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (Exception e) {
@@ -51,58 +64,5 @@ public class UnitAESTest {
         }
 
     }
-
-    @Test
-    public void Encryption2_isCorrect() {
-        try {
-            byte[] deans2 = AESHelper.decrypt(iv, key2, ans2);
-            assertEquals(msg, new String(deans2, "UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    @Test
-    public void EncryptionBase64_isCorrect() {
-        try {
-            deansBase64 = AESHelper.decryptStrAndFromBase64(iv, key, ansBase64);
-            assertEquals(msg, deansBase64);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void EncryptionBytes_isCorrect() {
-        try {
-            deByteAns = AESHelper.decrypt(bIv, bKey, bAns);
-            assertEquals(bMsg, deByteAns);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /*
-    Ask
-     */
-    @Test
-    public void EncryptionFiles_isCorrect() {
-        try {
-            AESHelper.encryptfile("path", key);
-            assertEquals(bMsg, deByteAns);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 
 }
